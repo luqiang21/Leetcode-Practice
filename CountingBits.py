25a8effd870efe0694fd36ca3810aa08d957c4ed
Qiang Lu <buaaluqiang@hotmail.com>
2017-04-25 20:16:52 -0600

    problem 338 Counting Bits 1st run

---
+++ CountingBits.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+'''
+338. Counting Bits
+Given a non negative integer number num.
+For every numbers i in the range 0 ≤ i ≤ num
+calculate the number of 1's in their binary representation and return them as an array.
+Example:
+For num = 5 you should return [0,1,1,2,1,2].
+
+Follow up:
+
+It is very easy to come up with a solution with run time O(n*sizeof(integer)).
+But can you do it in linear time O(n) /possibly in a single pass?
+Space complexity should be O(n).
+Can you do it like a boss?
+Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
+'''
+class Solution(object):
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        List = []
+        for i in range(num+1):
+            n_one = 0
+            while i > 0:
+                if i % 2 == 1:
+                    n_one += 1
+                i /= 2
+
+            List.append(n_one)
+
+        return List
+
+input = 2
+cB = Solution()
+print(cB.countBits(input))
