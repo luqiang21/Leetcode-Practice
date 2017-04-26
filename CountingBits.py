b1e6bb3433f7b02b56bb7e2ec40aa15819fd8b1d
Qiang Lu <buaaluqiang@hotmail.com>
2017-04-25 20:41:55 -0600

    using bit shift is so great! simple and fast.

--- CountingBits.py
+++ CountingBits.py
@@ -15,28 +15,25 @@ Space complexity should be O(n).
 Can you do it like a boss?
 Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
 '''
+
+'''
+一个数 * 2 就是把它的二进制全部左移一位，也就是说 1的个数是相等的。
+那么我们可以利用这个结论来做。
+res[i /2] 然后看看最低位是否为1即可（上面*2一定是偶数，这边比如15和14除以2都是7，但是15时通过7左移一位并且+1得到，14则是直接左移）
+所以res[i] = res[i >>1] + (i&1)
+
+'''
 class Solution(object):
     def countBits(self, num):
         """
         :type num: int
         :rtype: List[int]
         """
-        List = []
-        computed = {}
-        for i in range(num+1):
-            n_one = 0
-            while i > 0:
-                if i in computed:
-                    n_one += computed[i]
-                    i = 0
-                else:
-                    if i % 2 == 1:
-                        n_one += 1
-                    i /= 2
-            computed[i] = n_one
-            List.append(n_one)
+        List = [0] * (num + 1)
+        for i in range(1, num + 1):
+            List[i] = List[i >> 1] + i & 1
 
-        return List    
+        return List
 
 input = 2
 cB = Solution()
