86248ddd3b030ca3c80550c27f8c728171050527
Qiang Lu <buaaluqiang@hotmail.com>
2018-01-28 11:08:47 -0700

    complete Global and Local Inversions

--- tools.py
+++ tools.py
@@ -4,7 +4,7 @@ def timing(f):
         time1 = time.time()
         ret = f(*args)
         time2 = time.time()
-        print('\nThe args are', args)
+        # print('\nThe args are', args)
         print('%s function took %0.3f ms' % (f.__name__, (time2-time1)*1000.0))
         return ret
     return wrap
