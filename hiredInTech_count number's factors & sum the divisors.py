6e9abca262f2ef318aa00956dc7997b6c1f44b83
Qiang Lu <buaaluqiang@hotmail.com>
2018-02-14 11:49:05 -0700

    complete sum the divisors in hiredintech

--- hiredInTech_count number's factors & sum the divisors.py
+++ hiredInTech_count number's factors & sum the divisors.py
@@ -37,3 +37,38 @@ def count_numbers_factors(n):
 assert count_numbers_factors(1) == 1
 assert count_numbers_factors(2) == 2
 assert count_numbers_factors(12) == 6
+
+"""
+Given an integer n, calculate the sum of all divisors of n.
+
+For example, the divisors of 8 are 1,2,4 and 8 and 1 + 2 + 4 + 8 = 15
+The divisors of 7 are 1 and 7, which makes the sum 8.
+
+The input number n will be in the range [1, 10^9].
+
+Return one number - the sum of divisors of n.
+
+Sample test examples
+Input	Output
+8	15
+7	8
+1	1
+1000	2340
+"""
+@timing
+def sum_the_divisors(n):
+    # Write your code here
+    sum = 0
+    for i in range(1, int(n**0.5 + 1)):
+        if n%i == 0:
+            if n//i == i:
+                sum += i
+            else:
+                sum += i + n//i
+    return sum
+
+assert sum_the_divisors(8) == 15
+assert sum_the_divisors(7) == 8
+assert sum_the_divisors(1) == 1
+assert sum_the_divisors(1000) == 2340
+print('All tests passed.')
