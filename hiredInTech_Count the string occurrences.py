adbd899398580caf82df801608983519a9626cdf
Qiang Lu <buaaluqiang@hotmail.com>
2018-02-17 10:18:59 -0700

    complete count the string occurrences in hiredInTech with Rabin-Karp

--- hiredInTech_Count the string occurrences.py
+++ hiredInTech_Count the string occurrences.py
@@ -25,6 +25,7 @@ from tools import timing
 
 @timing
 def count_brute_force(T, P):
+    # complexity O((n-m+1)m), n = len(T), m = len(P)
     count = 0
     k = len(P)
     for i in range(len(T) - len(P) + 1):
@@ -34,9 +35,52 @@ def count_brute_force(T, P):
 
 @timing
 def count1(T, P):
-    # use KMP
+    # use Rabin-Karp, Processing time: theta(m), matching time: O((n-m+1)m),
+    # based on certain assumptions, average-case running time is better
+    count = 0
 
+    n = len(T)
+    m = len(P)
+    d = 26
+    q = 101
+    h = d**(m-1) % q
+    p = 0
+    t = 0
+    for i in range(m):
+        p = (d*p + ord(P[i]))%q
+        t = (d*t + ord(T[i]))%q
+
+    for s in range(n-m+1):
+        # print('substring', P, T[s:s+m])
+        if p == t:
+            if P == T[s:s+m]:
+                count += 1
+        if s < n-m:
+            # print('adding', s+m, 'deleting', s)
+            t = (d*(t - ord(T[s])*h) + ord(T[s+m])) % q
+        # print(p, t)
+    # print(count)
+    return count
+
+
+@timing
+def count2(T, P):
+    # use finite automata
+    pass
+
+@timing
+def count3(T, P):
+    # use KMP
+    pass
 
 T = 'babalabalabalatheend'
 P = 'alabala'
 assert count_brute_force(T, P) == 2
+assert count1(T, P) == 2
+T = "xxxxx"
+P = "xx"
+assert count_brute_force(T, P) == 4
+assert count1(T, P) == 4
+T, P = "3141592653589793", "26"
+assert count_brute_force(T, P) == 1
+assert count1(T, P) == 1
