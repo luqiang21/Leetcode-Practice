dc5f1762d0a719e9bd04244af8e38d0c9a0b46a9
Qiang Lu <buaaluqiang@hotmail.com>
2018-03-20 16:51:54 -0600

    add dp solution to regex parser

--- pramp_Basic Regex Parser.py
+++ pramp_Basic Regex Parser.py
@@ -39,6 +39,7 @@ Constraints:
 
 from tools import timing
 
+# recursive
 @timing
 def is_match(text, pattern):
     if not pattern:
@@ -52,7 +53,41 @@ def is_match(text, pattern):
     else:
         return first_match and is_match(text[1:], pattern[1:])
 
+# DP, time: O(NM), space O(NM)
+@timing
+def is_match1(text, pattern):
+    n, m = len(text), len(pattern)
+    match = [[False for _ in range(m+1)] for _ in range(n+1)]
+
+    match[0][0] = True # empty text and empty pattern matches
+
+    # empty text and nonempty pattern
+    for j in range(1, m+1):
+        if j > 1 and pattern[j-1] == '*':
+            match[0][j] = match[0][j-2]
+
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            # if i-1 th text and j-1 th pattern match, or j-1 th pattern is a '.'
+            if text[i-1] == pattern[j-1] or pattern[j-1] == '.':
+                match[i][j] = match[i-1][j-1]
+            elif j > 1 and pattern[j-1] == '*':
+                # if the char before * matches at least once, i.e. equal char or
+                # pattern is '.'
+                # match[i][j-2] is for matching 0, cause we need to skip the last 2
+                # characters in the pattern. match[i - 1][j] is for matching 1, cause
+                # we need to skip 1 character in the string, but keep the entire pattern
+                if pattern[j-2] == '.' or text[i-1] == pattern[j-2]:
+                    match[i][j] = match[i][j-2] or match[i-1][j]
+
+                # if doesn't match, the only way is 0 matched
+                else:
+                    match[i][j] = match[i][j-2]
+
+    return match[n][m]
+
 text = "acd"
 pattern = "ab*c."
 ans = True
 assert is_match(text, pattern) == ans
+assert is_match1(text, pattern) == ans
