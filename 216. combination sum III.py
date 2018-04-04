8d52219c84eb86f33ef737af3742c73fb46ddb5b
Qiang Lu <buaaluqiang@hotmail.com>
2018-04-04 17:14:06 -0600

    backtracking: 216 combination sum III

---
+++ 216. combination sum III.py
@@ -0,0 +1,56 @@
+"""
+Find all possible combinations of k numbers that add up to a number n, given
+that only numbers from 1 to 9 can be used and each combination should be a
+unique set of numbers.
+
+
+Example 1:
+
+Input: k = 3, n = 7
+
+Output:
+
+[[1,2,4]]
+
+Example 2:
+
+Input: k = 3, n = 9
+
+Output:
+
+[[1,2,6], [1,3,5], [2,3,4]]
+
+"""
+
+from tools import timing
+
+class Solution:
+    @timing
+    def combinationSum3(self, k, n):
+        """
+        :type k: int
+        :type n: int
+        :rtype: List[List[int]]
+        """
+
+        res = []
+        temp = []
+        idx = 1
+        self.backtracking(res, temp, n, k, idx)
+
+        return res
+
+    def backtracking(self, res, temp, n, k, idx):
+        if len(temp) == k and n == 0:
+            res.append(temp[:])
+            return
+
+        for i in range(idx, 10):
+            temp.append(i)
+            self.backtracking(res, temp, n - i, k, i + 1)
+            temp.pop()
+
+k, n = 3, 9
+ans = [[1,2,6], [1,3,5], [2,3,4]]
+
+assert Solution().combinationSum3(k, n) == ans
