6c521027fc3bbb57e6335b612f09f3ed6df6e914
Qiang Lu <buaaluqiang@hotmail.com>
2018-04-10 09:32:14 -0600

    add divide and conquer approach for arrayMaxConsecutiveSum2

--- codefights_arrayMaxConsecutiveSum2.py
+++ codefights_arrayMaxConsecutiveSum2.py
@@ -43,5 +43,60 @@ def arrayMaxConsecutiveSum2(inputArray):
 
     return max_so_far
 
+
+# A Divide and Conquer based program
+# for maximum subarray sum problem
+
+# Find the maximum possible sum in
+# arr[] such that arr[m] is part of it
+def maxCrossingSum(arr, l, m, h) :
+    # l: low, m: middle, h: high
+    # Include elements on left of mid.
+    sm = 0; left_sum = -10000
+
+    for i in range(m, l-1, -1) :
+        sm = sm + arr[i]
+
+        if (sm > left_sum) :
+            left_sum = sm
+
+
+    # Include elements on right of mid
+    sm = 0; right_sum = -10000
+    for i in range(m + 1, h + 1) :
+        sm = sm + arr[i]
+
+        if (sm > right_sum) :
+            right_sum = sm
+
+
+    # Return sum of elements on left and right of mid
+    return left_sum + right_sum;
+
+@timing
+# Returns sum of maxium sum subarray in aa[l..h]
+def arrayMaxConsecutiveSum(arr, l, h) :
+
+    # Base Case: Only one element
+    if (l == h) :
+        return arr[l]
+
+    # Find middle point
+    m = (l + h) // 2
+
+    # Return maximum of following three possible cases
+    # a) Maximum subarray sum in left half
+    # b) Maximum subarray sum in right half
+    # c) Maximum subarray sum such that the
+    #     subarray crosses the midpoint
+    return max(arrayMaxConsecutiveSum(arr, l, m),
+               arrayMaxConsecutiveSum(arr, m+1, h),
+               maxCrossingSum(arr, l, m, h))
+
+
+
+
+
 inputArray = [1, -2, 3, -4, 5, -3, 2, 2, 2]
 assert arrayMaxConsecutiveSum2(inputArray) == 8
+assert arrayMaxConsecutiveSum(inputArray, 0, len(inputArray)-1) == 8
