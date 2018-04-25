39985dc8735f7b91b60df86d8569de926b8d0056
Qiang Lu <buaaluqiang@hotmail.com>
2018-04-25 15:57:00 -0600

    Add 337. super house rob

---
+++ 337. House Robber III.py
@@ -0,0 +1,87 @@
+"""
+
+The thief has found himself a new place for his thievery again. There is only
+one entrance to this area, called the "root." Besides the root, each house has
+one and only one parent house. After a tour, the smart thief realized that "all
+houses in this place forms a binary tree". It will automatically contact the
+police if two directly-linked houses were broken into on the same night.
+
+Determine the maximum amount of money the thief can rob tonight without alerting
+the police.
+
+Example 1:
+     3
+    / \
+   2   3
+    \   \
+     3   1
+Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+Example 2:
+     3
+    / \
+   4   5
+  / \   \
+ 1   3   1
+Maximum amount of money the thief can rob = 4 + 5 = 9.
+"""
+from tools import timing
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+mem = {}
+
+class Solution:
+    @timing
+    def rob1(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+
+        # compute val which is root.val + next not directly linked nodes
+        val = root.val
+        if root.left:
+            val += self.rob1(root.left.left) + self.rob1(root.left.right)
+
+        if root.right:
+            val += self.rob1(root.right.left) + self.rob1(root.right.right)
+
+        return max(val, self.rob1(root.left) + self.rob1(root.right))
+
+    @timing
+    def rob(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+
+        if root in mem:
+            return mem[root]
+
+        # compute val which is root.val + next not directly linked nodes
+        val = root.val
+        if root.left:
+            val += self.rob(root.left.left) + self.rob(root.left.right)
+
+        if root.right:
+            val += self.rob(root.right.left) + self.rob(root.right.right)
+
+        mem[root] = max(val, self.rob(root.left) + self.rob(root.right))
+        return max(val, self.rob(root.left) + self.rob(root.right))
+
+root = TreeNode(3)
+root.left = TreeNode(4)
+root.right = TreeNode(5)
+root.left.left = TreeNode(1)
+root.left.right = TreeNode(3)
+root.right.right = TreeNode(1)
+
+assert Solution().rob1(root) == 9
+assert Solution().rob(root) == 9
