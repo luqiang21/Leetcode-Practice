4d1502fef407c95c4d2ecd6725fcba9b66233cf3
Qiang Lu <buaaluqiang@hotmail.com>
2018-07-17 15:15:13 -0600

    add heapsort implementation to 215 in cpp

--- 215. Kth Largest Element in an Array.cpp
+++ 215. Kth Largest Element in an Array.cpp
@@ -21,6 +21,48 @@ public:
 
         return pq.top();
     }
+
+private:
+    int heap_size;
+
+public:
+    // implement heapsort
+    inline int left(int idx) {return (idx << 1) + 1;}
+    inline int right(int idx) {return (idx << 1) + 2;}
+    inline int parent(int idx) {return (idx - 1) / 2;}
+
+    void max_heapify(vector<int>& nums, int idx) {
+        int largest = idx;
+        int l = left(idx), r = right(idx);
+        if(l < heap_size && nums[l] > nums[largest]) largest = l;
+        if(r < heap_size && nums[r] > nums[largest]) largest = r;
+
+        if(largest != idx) {
+            swap(nums[idx], nums[largest]);
+            max_heapify(nums, largest);
+        }
+
+    }
+
+    void build_max_heap(vector<int>& nums) {
+        heap_size = nums.size();
+
+        for(int i = (heap_size >> 1) - 1; i >= 0; i --) {
+            max_heapify(nums, i);
+        }
+    }
+
+    int findKthLargest(vector<int>& nums, int k) {
+        build_max_heap(nums);
+
+        for(int i = 0; i < k; i++) {
+            swap(nums[0], nums[heap_size - 1]);
+            heap_size --;
+            max_heapify(nums, 0);
+        }
+        return nums[heap_size];
+    }
+
 };
 
 int main() {
@@ -29,5 +71,7 @@ int main() {
     int k = 4;
 
     assert(sol.findKthLargest2(nums, k) == 4);
-    cout << "test passed" << endl;
+    assert(sol.findKthLargest(nums, k) == 4);
+
+    cout << "tests passed!" << endl;
 }
