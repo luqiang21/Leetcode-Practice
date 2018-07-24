ab6572ca1807081d7e9359d76b3f8ee064ef9e0e
Qiang Lu <buaaluqiang@hotmail.com>
2018-07-24 09:39:40 -0600

    add dp method for 72. edit distance in cpp

--- 72. edit distance.cpp
+++ 72. edit distance.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 int min(int x, int y, int z) {
     return min(min(x, y), z);
 }
 class Solution {
-public:
-
-
     int edit_distance(string s1, string s2, int m, int n) {
         // if first string is empty, only option is insert all chars in s2.
         if (m == 0) return n;
@@ -35,6 +33,37 @@ public:
     int edit_distance(string s1, string s2) {
         return edit_distance(s1, s2, s1.length(), s2.length());
     }
+
+    int edit_distance_dp(string s1, string s2) {
+        int m = s1.length(), n = s2.length();
+        vector< vector<int> > dp(m+1, vector<int>(n+1, 0));
+
+        for (int i = 0; i < m + 1; ++i) {
+            for (int j = 0; j < n + 1; ++j) {
+                if (i == 0) {
+                    // if no chars in s1, insert all chars of s2
+                    dp[i][j] = j;
+                }
+                else if (j == 0) {
+                    // if no chars in s2, remove all chars in s1
+                    dp[i][j] = i;
+                }
+                else if (s1[i-1] == s2[j-1]) {
+                    // if last chars are same, ignore them and recur for remaining
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else {
+                    // if not same, consider all possibilities and find min
+                    dp[i][j] = 1 + min(dp[i][j-1], // insert
+                                       dp[i-1][j], // remove
+                                       dp[i-1][j-1] // replace
+                                   );
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
 };
 
 int main() {
@@ -42,6 +71,10 @@ int main() {
     string str2 = "saturday";
 
     Solution sol;
-    cout << sol.edit_distance( str1 , str2) << endl;
+    cout << "The edit distance between " << str1 << " and " << str2 << " is " <<
+    sol.edit_distance( str1 , str2) << endl;
+
+    cout << "The edit distance between " << str1 << " and " << str2 << " is " <<
+    sol.edit_distance_dp( str1 , str2) << endl;
     return 0;
 }
