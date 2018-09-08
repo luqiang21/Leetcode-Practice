d374a06112172d9c5433e0aa2dfed4bc695a0482
Qiang Lu <buaaluqiang@hotmail.com>
2018-09-08 08:47:25 -0600

    add a simpler method to count wait using thread

--- python_packages/_thread_count_wait.py
+++ python_packages/_thread_count_wait.py
@@ -1,4 +1,5 @@
 import _thread as thread
+import time
 
 def counter(myID, count):
     for i in range(count):
@@ -10,7 +11,7 @@ def counter(myID, count):
 
 stdoutmutex = thread.allocate_lock()
 exitmutexes = []
-
+start = time.time()
 for i in range(10):
     exitmutexes.append(thread.allocate_lock())
     thread.start_new(counter, (i, 3))
@@ -18,3 +19,26 @@ for i in range(10):
 for mutex in exitmutexes:
     while not mutex.locked(): pass
 print("main process exiting")
+print("time took:", time.time() - start)
+print()
+
+# another version, simply change an integer instead of locks
+exitmutexes = [0]*10
+def counter(myID, count):
+    for i in range(count):
+        stdoutmutex.acquire()
+        # time.sleep(1)
+        print(myID, "=>", i)
+        stdoutmutex.release()
+    exitmutexes[myID] = 1 # signal main thread
+
+stdoutmutex = thread.allocate_lock()
+start = time.time()
+for i in range(10):
+    exitmutexes.append(thread.allocate_lock())
+    thread.start_new(counter, (i, 3))
+
+while 0 in exitmutexes: pass
+
+print("main process exiting")
+print("time took:", time.time() - start)
