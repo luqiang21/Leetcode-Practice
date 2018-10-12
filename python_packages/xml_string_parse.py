c518f7ddab106881779f64067e58939b6cc90242
Qiang Lu <buaaluqiang@hotmail.com>
2018-10-11 21:20:23 -0700

    add 904 fruit into baskets, same as 159, sliding window

--- python_packages/xml_string_parse.py
+++ python_packages/xml_string_parse.py
@@ -1,10 +1,11 @@
 import xml.dom.minidom
+import os
 
 xml_string = """<?xml version="1.0"?>
 <launch>
-  <basedir path="/home/sommer/src/inet/examples/erlangen6" />
+  <basedir path="/Users/LuQiang/Dropbox/Courses/Leetcode-Practice/python_packages" />
   <seed value="1234" />
-  <copy file="net.net.xml" />
+  <copy file="example.net.xml" />
   <copy file="routes.rou.xml" />
   <copy file="sumo.sumo.cfg" type="config" />
 </launch>
@@ -18,9 +19,23 @@ print(dir(p))
 print("tag name:", p.documentElement.tagName)
 launch_node = p.documentElement
 basedir_nodes = [x for x in launch_node.getElementsByTagName("basedir") if x.parentNode==launch_node]
-print("basedir nodes:", basedir_nodes)
+basedir = basedir_nodes[0].getAttribute("path")
+print("basedir nodes:", basedir_nodes, "basedir:", basedir)
 print("path:", p.documentElement.getElementsByTagName("basedir")[0].getAttribute("path"))
 print("seed:", launch_node.getElementsByTagName("seed")[0].getAttribute("value"))
 
 copy_nodes = [x for x in launch_node.getElementsByTagName("copy") if x.parentNode == launch_node]
 print(copy_nodes)
+
+print("copy_nodes[0] has file attribute?", copy_nodes[0].hasAttribute("file"))
+
+file_src_name = copy_nodes[0].getAttribute("file")
+file_src_path = os.path.join(basedir, file_src_name)
+print("file_src_path:", file_src_path)
+# read file content
+file_handle = open(file_src_path, 'rb')
+file_contents = file_handle.read()
+file_handle.close()
+print(file_contents)
+
+print()
