045d73e6045b520baf0f247b8dc5b78a4edd347a
Qiang Lu <buaaluqiang@hotmail.com>
2019-11-02 17:20:17 -0600

    101 symmetric tree_easy with recursive and iterative methods

---
+++ 101. Symmetric Tree_easy.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+//Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+//
+//For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+//
+//    1
+//   / \
+//  2   2
+// / \ / \
+//3  4 4  3
+//
+//
+//But the following [1,2,2,null,3,null,3] is not:
+//
+//    1
+//   / \
+//  2   2
+//   \   \
+//   3    3
+//
+//
+//Note:
+//Bonus points if you could solve it both recursively and iteratively.
+
+//**
+// * Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class SolutionRecursive {
+public:
+    bool isSymmetric(TreeNode* left, TreeNode* right) {
+        if (left == NULL && right == NULL) return true;
+        if (left == NULL || right == NULL) return false;
+
+        if (left -> val == right -> val) return isSymmetric(left -> left, right -> right) && isSymmetric(left -> right, right -> left);
+        return false;
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if (root == NULL) return true;
+
+        return isSymmetric(root -> left, root -> right);
+    }
+};
+
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (root == NULL) return true;
+        
+        deque<TreeNode*> queue;
+        queue.push_back(root -> left);
+        queue.push_back(root -> right);
+        
+        while (!queue.empty()) {
+            TreeNode* curLeft = queue.front();
+            queue.pop_front();
+            TreeNode* curRight = queue.front();
+            queue.pop_front();
+            
+            if (curLeft == NULL && curRight == NULL) continue;
+            if (curLeft == NULL || curRight == NULL || curLeft -> val != curRight -> val) return false;
+            
+            queue.push_back(curLeft -> left);
+            queue.push_back(curRight -> right);
+            queue.push_back(curLeft -> right);
+            queue.push_back(curRight -> left);
+        }
+        
+        return true;
+    }
+};
+
+int main() {
+	TreeNode* root1 = new TreeNode(1);
+	root1 -> left = new TreeNode(2);
+	root1 -> right = new TreeNode(2);
+	root1 -> left -> left = new TreeNode(3);
+	root1 -> left -> right = new TreeNode(4);
+	root1 -> right -> left = new TreeNode(4);
+	root1 -> right -> right = new TreeNode(3);
+
+	TreeNode* root2 = new TreeNode(1);
+	root2 -> left = new TreeNode(2);
+	root2 -> right = new TreeNode(2);
+	root2 -> left -> right = new TreeNode(3);
+	root2 -> right -> right = new TreeNode(3);
+
+	assert(SolutionRecursive().isSymmetric(root1) == true);
+	assert(SolutionRecursive().isSymmetric(root2) == false);
+	assert(Solution().isSymmetric(root1) == true);
+	assert(Solution().isSymmetric(root2) == false);
+	cout << "All tests passed!" << endl;
+	return 0;
+}
